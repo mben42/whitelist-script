94e6da161db57ba45a858ab731d95b51f0859f77
mathis benguigui <mathis.benguigui@epitech.eu>
2021-03-12 21:53:13 +0100

    Increasing delay between commands, triggers some anti-spam systems, fixing it to 5 seconds to avoid any problem

--- whitelist.py
+++ whitelist.py
@@ -10,4 +10,4 @@ for nick in userlist:
     keyboard.write(f"{command} {nick}")
     pyautogui.press('enter')
     pyautogui.press('t')
-    time.sleep(1) # 1 second sleep not to trigger anti-spam systems
+    time.sleep(5) # 5 seconds sleep not to trigger anti-spam systems
